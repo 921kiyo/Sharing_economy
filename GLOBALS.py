09c71e0c92f4a751bc8016436b117a33d6197aef
John Lingi <lingijohn23@gmail.com>
2018-10-20 12:42:14 +0200

    returns products on user homepage

---
+++ GLOBALS.py
@@ -0,0 +1,23 @@
+CONDITION = {
+    0: "poor",
+    1: "good",
+    2: "great"
+}
+
+
+PRODUCT_LISTINGS = {
+
+    "products": [
+        {
+            "name": "Water T-shirt",
+            "seller_id": 1,
+            "description": "Beautiful T-shirt with a water logo on it.",
+            "image": 1,  # Dummy
+            "price": 26,  # Dollars
+            "categories": ["apparel", "boys"],
+            "condition": "good",  # Should this be an integer
+            "size": "XL",
+            "shipping_cost": 0
+        }
+    ]
+}
--- main.py
+++ main.py
@@ -1,15 +1,22 @@
+# GLOBALS
+from GLOBALS import PRODUCT_LISTINGS
 
 from flask import Flask, render_template, url_for, redirect, request \
     , flash, jsonify, make_response, session as login_session
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    # return render_template("/index.html", hello="Hello world!")
-    foo = [1,2,3]
-    return render_template("/index.html", hello=foo)
-    # return 'Hello, World!'
+    """
+    Returns product listings for user homepage
+
+    :return:
+    """
+
+    print(PRODUCT_LISTINGS)
+    return jsonify(data=PRODUCT_LISTINGS)
+
 
 if __name__ == '__main__':
     app.secret_key = 'secret'
