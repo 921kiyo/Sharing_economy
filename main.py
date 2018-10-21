fcdece055bb2719969638f046b324b1cc6738393
John Lingi <lingijohn23@gmail.com>
2018-10-21 04:56:11 +0200

    added delete when buy product

--- main.py
+++ main.py
@@ -128,13 +128,13 @@ def user_home(user_id):
             user.donating = True
             user.charity_id = charity_id
             # charity.num_donators = charity.num_donators + 1
-            flash('Thank you for much for helping our charity partner!!', 'success')
+            # flash('Thank you for much for helping our charity partner!!', 'success')
 
         else:
             charity = session.query(Charity).filter_by(id=user.charity_id).first()
             user.donating = False
             # charity_id.num_donators = charity.num_donators - 1
-            flash("Sad you are leaving the donation", "error")            
+            # flash("Sad you are leaving the donation", "error")
         session.commit()
 
     # Returns the list of products by the user
@@ -211,7 +211,10 @@ def buy_product(product_id):
 
         print("Total donated to date is", user.total_donated, "to", charity.name)
 
+    # Remove product from product table
+    session.delete(product)
     session.commit()
+
     flash('Purchase complete. Thank you so much!', 'success')
 
     products = session.query(Product).all()
